89729e696ff671748cb1fc317cde0f3f449ca6ca
Nogive <603446367@qq.com>
2018-12-05 13:28:25 +0800

    添加 quickarray

--- src/mmform/index.js
+++ src/mmform/index.js
@@ -4,14 +4,16 @@ import mmInput from "./components/mmInput";
 import mmNumber from "./components/mmNumber";
 import mmTextarea from "./components/mmTextarea";
 import mmRadio from "./components/mmRadio";
-import mmSelect from "./components/mmSelect_axios";
+import mmSelect from "./components/mmSelect";
 import mmDatePicker from "./components/mmDatePicker";
 import mmPhoto from "./components/mmPhoto";
 import mmLocation from "./components/mmLocation";
 import mmObject from "./components/mmObject";
 import mmArray from "./components/mmArray";
 import mmArea from "./components/mmArea";
-import mmForeignObject from "./components/mmForeignObject_axios";
+import mmForeignObject from "./components/mmForeignObject";
+import mmQuickArray from "./components/mmQuickArray";
+import mmSimpleArray from "./components/mmSimpleArray";
 
 Vue.use(vueNcform, {
   extComponents: {
@@ -26,6 +28,9 @@ Vue.use(vueNcform, {
     mmObject,
     mmArray,
     mmArea,
-    mmForeignObject
+    mmForeignObject,
+    mmQuickArray,
+    mmSimpleArray
   }
 });
+
